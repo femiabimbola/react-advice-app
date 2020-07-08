897c119ca5322ff3bb77e1a1022af4c0aa45c872
unknown <femiabimbola6@gmail.com>
2020-07-08 23:57:20 +0100

    chore:added axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import axios from 'axios';
+import './App.css';
 
 class App extends React.Component {
+
+    fetchAdviceApi = () => {
+        axios.get()
+            .then()
+    }
 
     render(){
         return (
